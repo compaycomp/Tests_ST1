c464b7959fd6f00b14922a0805cd3395bc2902cb
compaycomp <compaycpmp@gmail.com>
2024-04-21 17:54:53 +0300

    Добавьте файлы проекта.

---
+++ Tests_ST1.Tests/PingTests/Class1Tests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using Tests_ST1.Ping;
+
+namespace Tests_ST1.Tests.PingTests
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            int result = Calculator.Add(5, 3);
+            int expected = 8;
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void TestSubtract()
+        {
+            int result = Calculator.Subtract(5, 3);
+            int expected = 2;
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Tests_ST1.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tests_ST1.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
